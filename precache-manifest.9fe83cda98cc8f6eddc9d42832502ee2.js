dec5a610178631efef287798e5f93ab27d274d0a
kalanakbr <kalanakr@gmail.com>
2019-08-03 10:12:43 +0530

    Updates

---
+++ precache-manifest.9fe83cda98cc8f6eddc9d42832502ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df230e922b09f7c196dd9dd1f868dec",
+    "url": "/Hostel-Management-System/index.html"
+  },
+  {
+    "revision": "04de8bd9410c641efa1d",
+    "url": "/Hostel-Management-System/static/css/main.2582ba8f.chunk.css"
+  },
+  {
+    "revision": "b714309e77b5cba8fd66",
+    "url": "/Hostel-Management-System/static/js/2.8ec9c194.chunk.js"
+  },
+  {
+    "revision": "04de8bd9410c641efa1d",
+    "url": "/Hostel-Management-System/static/js/main.937e800d.chunk.js"
+  },
+  {
+    "revision": "ad9e78d9427b10f805c3",
+    "url": "/Hostel-Management-System/static/js/runtime~main.8497fb85.js"
+  },
+  {
+    "revision": "0e1658017a2da40778f58ccceacc969f",
+    "url": "/Hostel-Management-System/static/media/fos.0e165801.jpg"
+  }
+]);
